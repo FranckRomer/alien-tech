519718d97d13fcff243e62d437db715df217a01a
FranckRomer <paco_metal95@hotmail.com>
2023-04-17 18:52:04 -0600

    inicial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
+    "./sections/**/*.{js,ts,jsx,tsx}",
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
